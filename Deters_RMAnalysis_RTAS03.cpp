bdfd3598ebe26fbf5477f61690b284d641958e79
Son Dinh <dinhs@objectcomputing.com>
2025-09-16 08:50:51 -0500

    - Add RM implementation from Deters' paper - Add implementation for QPA

---
+++ Deters_RMAnalysis_RTAS03.cpp
@@ -0,0 +1,168 @@
+#include <loki/Typelist.h>
+
+using namespace Loki;
+
+template <class TaskSet>
+struct Schedule;
+
+template <class Head, class Tail>
+struct Schedule<Typelist<Head, Tail>>
+{
+  typedef Typelist<Head, Tail> TL;
+
+  enum
+  {
+    feasible = RMA_Feasible<TL>::Result
+  };
+
+  static const double utilization = Schedule<Tail>::utilization + double(Head::cost) / Head::period;
+
+  static void schedule(void)
+  {
+    /* (not shown) */
+  }
+};
+
+template <>
+struct Schedule<NullType>
+{
+  static const bool Result = true;
+  static const double utilization = 0.0;
+
+  static void schedule(void)
+  {
+    // no action necessary
+  }
+};
+
+template <class TL, int m, int i>
+struct check_i;
+
+// Recursive case for check_i
+template <class Head, class Tail, int m, int i>
+struct check_i<Typelist<Head, Tail>, m, i>
+{
+  enum
+  {
+    task_result = task_feasible<Typelist<Head, Tail>, i>::Result,
+
+    Result = check_i<Typelist<Head, Tail>, m, i + 1>::Result && task_result
+  };
+};
+
+// Base case for check_i
+template <class Head, class Tail, int m>
+struct check_i<Typelist<Head, Tail>, m, m>
+{
+  enum
+  {
+    Result = task_feasible<Typelist<Head, Tail>, m>::Result
+  };
+};
+
+template <class TaskSet>
+struct RMA_Feasible
+{
+  enum
+  {
+    m = TL::Length<TaskSet>::value,
+
+    Result = check_i<TaskSet, m, 1>::Result
+  };
+};
+
+template <class TL, int i, int t, int j = 0>
+struct sum_j
+{
+  typedef typename TypeAt<TL, j>::Result J;
+
+  enum
+  {
+    Cj = J::cost,
+
+    Tj = J::period,
+
+    my_result = Cj * ((t % Tj > 0 ? 1 : 0) + (t / Tj)),
+
+    Result = sum_j<TL, i, t, j + 1>::Result + my_result
+  };
+};
+
+template <class TL, int i, int t>
+struct sum_j<TL, i, t, i>
+{
+  enum
+  {
+    Result = 0
+  };
+};
+
+template <class TL, int i, int t_ix, int k = 0>
+struct get_t
+{
+  enum
+  {
+    Ti = TypeAt<TL, i - 1>::Result::period,
+
+    Tk = TypeAt<TL, k>::Result::period,
+
+    num_l = Ti / Tk,
+
+    Result = (t_ix >= num_l) ?
+      get_t<TL, i, t_ix - num_l, k + 1>::Result
+      : (t_ix + 1) * Tk
+  };
+};
+
+template <class TL, int i, int t_ix>
+struct get_t<TL, i, t_ix, i>
+{
+  enum
+  {
+    Result = 0
+  };
+};
+
+template <class TL, int i, int t_ix = 0>
+struct task_feasible
+{
+  typedef get_t<TL, i, t_ix> t_type;
+  enum
+  {
+    t = t_type::Result,
+
+    Result = (t > 0) &&
+             (sum_j<TL, i, t>::Result <= t || task_feasible<TL, i, t_ix + 1>::Result)
+  };
+};
+
+template <class TL, int i>
+struct task_feasible<TL, i, i>
+{
+  enum
+  {
+    Result = 0
+  };
+};
+
+// Example with two tasks
+int main() {
+  struct TaskA {
+    enum {
+      cost = 5,
+      period = 10
+    };
+  };
+
+  struct TaskB {
+    enum {
+      cost = 5,
+      period = 15
+    };
+  };
+
+  typedef LOKI_TYPELIST_2(TaskA, TaskB) MyTasks;
+  const int feasible = RMA_Feasible<MyTasks>::Result;
+
+  return 0;
+}
---
+++ Zhang_QPA_ECRTS09.cpp
@@ -0,0 +1,110 @@
+#include <loki/Typelist.h>
+
+using namespace Loki;
+
+// Example task format:
+// struct Task {
+//   enum {
+//     Wcet = 10, // worst-case execution time
+//     Deadline = 20, // relative deadline
+//     Period = 25  // period
+//   };
+// };
+
+// Total Wcet of all tasks would be TotalWcet<Taskset, n>,
+// where n is the number of tasks in the taskset, and
+// Taskset is a typelist of all tasks, e.g. LOKI_TYPELIST_3(Task1, Task2, Task3)
+template <class TList, unsigned int n>
+struct TotalWcet;
+
+template <class Head, class Tail, unsigned int i>
+struct TotalWcet<Typelist<Head, Tail>, i> {
+  enum {
+    Result = Head::Wcet + TotalWcet<Tail, i - 1>::Result
+  };
+};
+
+struct TotalWcet<NullType, 0> {
+  enum { Result = 0 };
+};
+
+// Compute L_b
+template <class TList, unsigned int iter>
+struct Lb;
+
+template <class TList, unsigned int iter, unsigned int i>
+struct LbHelper {
+  enum {
+    SumWcet = TotalWcet<TList, TL::Length<TList>::value>::Result,
+
+    PrevLb = Lb<TList, iter - 1>::Result,
+
+    Wcet = TList::Head::Wcet,
+    Period = TList::Head::Period,
+
+    MyValue = ((PrevLb % Period > 0 ? 1 : 0) + (PrevLb / Period)) * Wcet,
+
+    Result = MyValue + LbHelper<TList, iter, i - 1>::Result
+  };
+};
+
+template <class TList, unsigned int iter>
+struct LbHelper<TList, iter, 0> {
+  enum { Result = 0 };
+};
+
+// TODO: stop the recursion when Lb does not change anymore
+template <class TList, unsigned int iter>
+struct Lb {
+  enum {
+    n = TL::Length<TList>::value,
+
+    Result = LbHelper<TList, iter, n>::Result
+  };
+};
+
+template <class TList>
+struct Lb<TList, 0> {
+  enum {
+    Result = TotalWcet<TList, TL::Length<TList>::value>::Result
+  };
+};
+
+// TODO: static const values can be replaced with constexpr?
+
+// Compute L_a_star
+template <class TList, unsigned int i>
+struct Numerator {
+  static const int Wcet = TList::Head::Wcet;
+  static const int Deadline = TList::Head::Deadline;
+  static const int Period = TList::Head::Period;
+  static const double value = (static_cast<double>(Wcet) / Period) * (Period - Deadline) + Numerator<TList::Tail, i - 1>::value;
+};
+
+template <class TList, unsigned int i>
+struct TotalUtilization {
+  static const int Wcet = TList::Head::Wcet;
+  static const int Period = TList::Head::Period;
+  static const double value = (static_cast<double>(Wcet) / Period) + TotalUtilization<TList::Tail, i - 1>::value;
+};
+
+template <class TList, unsigned int i>
+struct LaStarHelper {
+  static const int Deadline = TList::Head::Deadline;
+  static const int Period = TList::Head::Period;
+  static const int tmp = Deadline - Period;;
+
+  static const int Result = tmp > LaStarHelper<TList::Tail, i - 1>::Result ? tmp : LaStarHelper<TList::Tail, i - 1>::Result;
+};
+
+struct LaStarHelper<NullType, 0> {
+  static const int Result = 0;
+};
+
+template <class TList>
+struct LaStar {
+  static const int n = TL::Length<TList>::value;
+  static const int LaUpperBound = static_cast<int>(Numerator<TList, n>::value / (1 - TotalUtilization<TList, n>::value));
+  static const int LaUpperBound2 = LaStarHelper<TList, n>::Result;
+  static const int Result = LaUpperBound2 > LaUpperBound ? LaUpperBound2 : LaUpperBound;
+};
