ccf2c3726a5661aa2389a78d0a18454874d78e9f
Son Dinh <dinhs@objectcomputing.com>
2026-03-16 01:20:55 -0500

    More complex task set fails

--- rust/src/qpa/type_based.rs
+++ rust/src/qpa/type_based.rs
@@ -1,7 +1,18 @@
 use core::ops::{Add, Sub, Div, Mul, BitAnd, Rem};
 use std::marker::PhantomData;
-use typenum::{Bit, False, Integer, P1, P5, P7, P10, P12, P14, P15, P100, P200, P1000000000000000000, True, Z0};
+use typenum::{Bit, False, Integer, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P12, P14, P15, P16, P17, P18, P19, P26, P31, P90, P96, P100, P160, P200, P280, P660, P800, P1000, P1000000000000000000, True, Z0};
 use typenum::{Sum, Diff, Prod, Quot, Mod, Min, Max, Maximum, IsLess, IsEqual, IsLessOrEqual, IsGreater, And};
+
+type P2000 = typenum::op!(P1000 * P2);
+type P3000 = typenum::op!(P1000 * P3);
+type P4200 = typenum::op!(P1000 * P4 + P200);
+type P6000 = typenum::op!(P1000 * P6);
+type P9000 = typenum::op!(P1000 * P9);
+type P9800 = typenum::op!(P1000 * P9 + P800);
+type P12000 = typenum::op!(P1000 * P12);
+type P17000 = typenum::op!(P1000 * P17);
+type P18000 = typenum::op!(P1000 * P18);
+type P31000 = typenum::op!(P1000 * P31);
 
 type PMax = P1000000000000000000;
 
@@ -353,7 +364,7 @@ where
 }
 
 #[test]
-fn test() {
+fn test1() {
     type PdfOf100 = <Taskset as Pdf<P100>>::Output;
     println!("h(100): {}", <PdfOf100 as Integer>::to_i32());
 
@@ -399,18 +410,91 @@ fn test() {
     type MinDeadline = <Taskset as Dmin>::Output;
     println!("Dmin: {}", <MinDeadline as Integer>::to_i32());
 
-    // Expected to be 12
     type MyLb = <(Task1, Tasklist<Task2, Nulltask>, Z0, SumWcet) as Lb>::Output;
+    assert_eq!(<MyLb as Integer>::to_i32(), 12);
     println!("Lb: {}", <MyLb as Integer>::to_i32());
 
-    // Expected to be 10
     type DmaxOf12 = <Taskset as Dmax<P12>>::Output;
+    assert_eq!(<DmaxOf12 as Integer>::to_i32(), 10);
     println!("Dmax(Lb=12): {}", <DmaxOf12 as Integer>::to_i32());
 
-    // Expected to be 5
     type PdfOf10 = <Taskset as Pdf<P10>>::Output;
+    assert_eq!(<PdfOf10 as Integer>::to_i32(), 5);
     println!("h(t=10): {}", <PdfOf10 as Integer>::to_i32());
 
     type MyQpa = <(Task1, Tasklist<Task2, Nulltask>, MyLb) as Qpa>::Output;
+    assert!(<MyQpa as Bit>::to_bool());
+    println!("Qpa: {}", <MyQpa as Bit>::to_bool());
+}
+
+#[test]
+fn test2() {
+    struct Task1;
+    impl Task for Task1 {
+        type Wcet = P6000;
+        type Deadline = P18000;
+        type Period = P31000;
+    }
+
+    struct Task2;
+    impl Task for Task2 {
+        type Wcet = P2000;
+        type Deadline = P9000;
+        type Period = P9800;
+    }
+
+    struct Task3;
+    impl Task for Task3 {
+        type Wcet = P1000;
+        type Deadline = P12000;
+        type Period = P17000;
+    }
+
+    struct Task4;
+    impl Task for Task4 {
+        type Wcet = P90;
+        type Deadline = P3000;
+        type Period = P4200;
+    }
+
+    struct Task5;
+    impl Task for Task5 {
+        type Wcet = P8;
+        type Deadline = P10;
+        type Period = P96;
+    }
+
+    struct Task6;
+    impl Task for Task6 {
+        type Wcet = P2;
+        type Deadline = P16;
+        type Period = P12;
+    }
+
+    struct Task7;
+    impl Task for Task7 {
+        type Wcet = P10;
+        type Deadline = P19;
+        type Period = P280;
+    }
+
+    struct Task8;
+    impl Task for Task8 {
+        type Wcet = P26;
+        type Deadline = P160;
+        type Period = P660;
+    }
+
+    type RemainingTasks = Tasklist<Task2, Tasklist<Task3, Tasklist<Task4, Tasklist<Task5, Tasklist<Task6, Tasklist<Task7, Tasklist<Task8, Nulltask>>>>>>>;
+    type Taskset = Tasklist<Task1, RemainingTasks>;
+
+    type SumWcet = <Taskset as TotalWcet>::Output;
+    let total_wcet = <SumWcet as Integer>::to_i32();
+    println!("Total wcet: {}", total_wcet);
+
+    type MyLb = <(Task1, RemainingTasks, Z0, SumWcet) as Lb>::Output;
+    println!("Lb: {}", <MyLb as Integer>::to_i32());
+
+    type MyQpa = <(Task1, RemainingTasks, MyLb) as Qpa>::Output;
     println!("Qpa: {}", <MyQpa as Bit>::to_bool());
 }
