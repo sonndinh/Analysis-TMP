db1e0005fe7e62561a92ab5b87bb6b2aefb2f089
Son Dinh <dinhs@objectcomputing.com>
2026-04-13 08:59:32 -0500

    Simplify resulting rational numbers

--- rust/src/common.rs
+++ rust/src/common.rs
@@ -182,6 +182,17 @@ fn test_max() {
 use std::ops::{Add, Sub, Mul, Div};
 use std::marker::PhantomData;
 
+trait GCD
+{
+    type Output;
+}
+
+// TODO: Compute the GCD of two integers A and B
+impl<A, B> GCD for (A, B)
+{
+    type Output = P1;
+}
+
 trait RationalNumber
 {
     type Numerator;
@@ -189,6 +200,20 @@ trait RationalNumber
 }
 
 struct Rational<N, D>(PhantomData<N>, PhantomData<D>);
+
+trait Reduce<F>
+{
+    type Output;
+}
+
+// Simplify the numerator and denominator by a factor F
+impl<N, D, F> Reduce<F> for Rational<N, D>
+where
+    N: Div<F>,
+    D: Div<F>
+{
+    type Output = Rational<<N as Div<F>>::Output, <D as Div<F>>::Output>;
+}
 
 impl<N, D> RationalNumber for Rational<N, D>
 {
@@ -205,15 +230,14 @@ where
     N1: Mul<D2>,
     N2: Mul<D1>,
     <N1 as Mul<D2>>::Output: Add<<N2 as Mul<D1>>::Output>,
-    D1: Mul<D2>
+    D1: Mul<D2>,
+    (AddNumerator<N1, D1, N2, D2>, AddDenominator<D1, D2>): GCD,
+    Rational<AddNumerator<N1, D1, N2, D2>, AddDenominator<D1, D2>>: Reduce<<(AddNumerator<N1, D1, N2, D2>, AddDenominator<D1, D2>) as GCD>::Output>
 {
-    // TODO:
-    // - Simplify the result by dividing the numerator and denominator by their greatest common divisor.
-    // - If D1 and D2 are the same, we only need to add the numerators.
-    type Output = Rational<AddNumerator<N1, D1, N2, D2>, AddDenominator<D1, D2>>;
+    type Output = <Rational<AddNumerator<N1, D1, N2, D2>, AddDenominator<D1, D2>> as Reduce<<(AddNumerator<N1, D1, N2, D2>, AddDenominator<D1, D2>) as GCD>::Output>>::Output;
 
     fn add(self, _other: Rational<N2, D2>) -> Self::Output {
-        Rational(PhantomData, PhantomData)
+        unimplemented!()
     }
 }
 
@@ -225,12 +249,14 @@ where
     N1: Mul<D2>,
     N2: Mul<D1>,
     <N1 as Mul<D2>>::Output: Sub<<N2 as Mul<D1>>::Output>,
-    D1: Mul<D2>
+    D1: Mul<D2>,
+    (SubNumerator<N1, D1, N2, D2>, SubDenominator<D1, D2>): GCD,
+    Rational<SubNumerator<N1, D1, N2, D2>, SubDenominator<D1, D2>>: Reduce<<(SubNumerator<N1, D1, N2, D2>, SubDenominator<D1, D2>) as GCD>::Output>
 {
-    type Output = Rational<SubNumerator<N1, D1, N2, D2>, SubDenominator<D1, D2>>;
+    type Output = <Rational<SubNumerator<N1, D1, N2, D2>, SubDenominator<D1, D2>> as Reduce<<(SubNumerator<N1, D1, N2, D2>, SubDenominator<D1, D2>) as GCD>::Output>>::Output;
 
     fn sub(self, _other: Rational<N2, D2>) -> Self::Output {
-        Rational(PhantomData, PhantomData)
+        unimplemented!()
     }
 }
 
@@ -240,12 +266,14 @@ type MulDenominator<D1, D2> = CommonDenominator<D1, D2>;
 impl<N1, D1, N2, D2> Mul<Rational<N2, D2>> for Rational<N1, D1>
 where
     N1: Mul<N2>,
-    D1: Mul<D2>
+    D1: Mul<D2>,
+    (MulNumerator<N1, N2>, MulDenominator<D1, D2>): GCD,
+    Rational<MulNumerator<N1, N2>, MulDenominator<D1, D2>>: Reduce<<(MulNumerator<N1, N2>, MulDenominator<D1, D2>) as GCD>::Output>
 {
-    type Output = Rational<MulNumerator<N1, N2>, MulDenominator<D1, D2>>;
+    type Output = <Rational<MulNumerator<N1, N2>, MulDenominator<D1, D2>> as Reduce<<(MulNumerator<N1, N2>, MulDenominator<D1, D2>) as GCD>::Output>>::Output;
 
     fn mul(self, _other: Rational<N2, D2>) -> Self::Output {
-        Rational(PhantomData, PhantomData)
+        unimplemented!()
     }
 }
 
@@ -255,24 +283,38 @@ type DivDenominator<D1, N2> = <D1 as Mul<N2>>::Output;
 impl<N1, D1, N2, D2> Div<Rational<N2, D2>> for Rational<N1, D1>
 where
     N1: Mul<D2>,
-    D1: Mul<N2>
+    D1: Mul<N2>,
+    (DivNumerator<N1, D2>, DivDenominator<D1, N2>): GCD,
+    Rational<DivNumerator<N1, D2>, DivDenominator<D1, N2>>: Reduce<<(DivNumerator<N1, D2>, DivDenominator<D1, N2>) as GCD>::Output>
 {
-    type Output = Rational<DivNumerator<N1, D2>, DivDenominator<D1, N2>>;
+    type Output = <Rational<DivNumerator<N1, D2>, DivDenominator<D1, N2>> as Reduce<<(DivNumerator<N1, D2>, DivDenominator<D1, N2>) as GCD>::Output>>::Output;
 
     fn div(self, _other: Rational<N2, D2>) -> Self::Output {
-        Rational(PhantomData, PhantomData)
+        unimplemented!()
     }
 }
 
-use typenum::{P1, P2};
+use typenum::{P1, P2, P4};
 use typenum::{Integer};
 
 #[test]
 fn test_rational_add() {
     type R1 = Rational<P1, P2>;
-    type R2 = R1;
-    type R3 = <R1 as Add<R2>>::Output;
+    type R2 = Rational<P1, P4>;
 
-    println!("Numerator: {}", <<R3 as RationalNumber>::Numerator as Integer>::to_i32());
-    println!("Denominator: {}", <<R3 as RationalNumber>::Denominator as Integer>::to_i32());
+    type SumResult = <R1 as Add<R2>>::Output;
+    println!("Numerator: {}", <<SumResult as RationalNumber>::Numerator as Integer>::to_i32());
+    println!("Denominator: {}", <<SumResult as RationalNumber>::Denominator as Integer>::to_i32());
+
+    type SubResult = <R1 as Sub<R2>>::Output;
+    println!("Numerator: {}", <<SubResult as RationalNumber>::Numerator as Integer>::to_i32());
+    println!("Denominator: {}", <<SubResult as RationalNumber>::Denominator as Integer>::to_i32());
+
+    type MulResult = <R1 as Mul<R2>>::Output;
+    println!("Numerator: {}", <<MulResult as RationalNumber>::Numerator as Integer>::to_i32());
+    println!("Denominator: {}", <<MulResult as RationalNumber>::Denominator as Integer>::to_i32());
+
+    type DivResult = <R1 as Div<R2>>::Output;
+    println!("Numerator: {}", <<DivResult as RationalNumber>::Numerator as Integer>::to_i32());
+    println!("Denominator: {}", <<DivResult as RationalNumber>::Denominator as Integer>::to_i32());
 }
